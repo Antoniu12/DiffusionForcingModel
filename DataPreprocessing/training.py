e21170a59bc9497ae94885d28c33be2353fe30bd
Antoniu12 <126654270+Antoniu12@users.noreply.github.com>
2025-06-03 18:00:30 +0300

    improved diffusion and sampling

--- DataPreprocessing/training.py
+++ DataPreprocessing/training.py
@@ -1,43 +1,31 @@
 import os
-import pickle
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import matplotlib
 import numpy as np
-import pandas as pd
-import torch
-from torch.utils.data import DataLoader
 
-from DiffusionBase.DF_Backbone_UNet import DFBackboneUNet
-from DiffusionBase.diffusion_forcing_sampling import sample_whole_test
-from models.Lstm import LSTMRegressor
-from models.TrainModel import train_model
-from models.Transformer import TransformerRegressor
-from models.forecast import forecast_day_from_model_aep, forecast_day_from_model_h
-from models.generate_predictions import predict_next_24h
-from utils.Sequence_Dataset import SequenceDataset
+from DiffusionBase.diffusion_forcing_sampling import sample_whole_test, generate_predictions
 
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 
 from utils.Logger import Logger
-from utils.utils import compute_crps, save_model, extract_target_series, flatten_overlapping_windows, \
-    flatten_overlapping_windows_last, get_from_sequence
+from utils.utils import compute_crps, save_model, flatten_overlapping_windows, \
+    get_from_sequence
 import torch
 print(torch.cuda.is_available())
 print(torch.cuda.get_device_name(0))
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
 
-from DataPreprocessing.preprocess import load_and_preprocess_data, create_sequences, create_tensors, \
-    plot_feature_correlation_heatmap, preprocess_aep_dataset
+from DataPreprocessing.preprocess import load_and_preprocess_data, create_sequences, create_tensors
 from utils import utils
-from DiffusionBase.DF_Backbone import DFBackbone, pretrain_layers
+from DiffusionBase.DF_Backbone import DFBackbone  # , pretrain_layers
 from DiffusionBase.df_training_v2 import df_training, predict, predict_with_uncertainty
-from plots import plot_test_predictions, plot_predictions_with_uncertainty, plot_diffusion_forecast
+from plots import plot_test_predictions, plot_predictions_with_uncertainty
 
 #sa nu incerc pe H13!!!!
-file_path = './training sets/H4_Wh.csv'
+file_path = './training sets/H1_Wh.csv'
 save_path = os.path.join("plots", datetime.now().strftime("%Y-%m-%d_%H-%M"))
 os.makedirs(save_path, exist_ok=True)
 logger = Logger(save_dir=save_path)
@@ -53,8 +41,8 @@ print(f"Number of training sequences: {len(train_sequences)}")
 print(f"Number of validation sequences: {len(validation_sequences)}")
 print(f"Number of test sequences: {len(test_sequences)}")
 input_dim = data_normalised.shape[1]
-hidden_dim = 512
-K = 100
+hidden_dim = 1024
+K = 1000
 epochs = 60
 betas = utils.cosine_beta_schedule(K)
 alpha, alpha_bar = utils.get_alphas(betas)
@@ -68,105 +56,42 @@ model_config = {
     'loss_type': "spike_and_small_masked",
 }
 # logger.log_model_config(model_name="DFBackbone", config_dict=model_config)
-# # device = 'cpu'
+# device = 'cpu'
 alpha = alpha.to(device)
 alpha_bar = alpha_bar.to(device)
 #
 forecast_window = 24
 # stride = 1
-
-
+#
 #LOAD MODEL FOR PREDICTION
 ##############################################################################################################################
-# # context_seq = create_sequences(test_tensor[-forecast_window], seq_length)
-# model = DFBackbone(input_dim=input_dim, hidden_dim=hidden_dim, seq_dim=seq_length)
-# model.load_state_dict(torch.load("plots/2025-05-16_17-25/df_model_2025-05-16_17-27-53.pt"))
-# model.to(device)
-#
-# # context_input = test_tensor[:-forecast_window]
-# # true_future = test_tensor[-forecast_window:]
-# #
-# # context_seq = create_sequences(context_input, seq_length)
-#
-# #
-# torch.cuda.empty_cache()
-# # forecast = sample_from_diffusion_with_context(
-# #     model, context_seq, seq_length, forecast_window, hidden_dim, alpha, alpha_bar, K
-# # )
-# test_sequences = create_sequences(test_tensor[-forecast_window:], seq_length)
-# predictions = sample_whole_test(model, test_sequences, alpha_bar, seq_length, hidden_dim, K, device)
-#
-# true = flatten_overlapping_windows(test_sequences)
-# forecast = flatten_overlapping_windows(predictions)
-#
-# forecast = get_from_sequence(forecast.detach(), column="Consumption").cpu().numpy()
-# true_future = get_from_sequence(true, column="Consumption").cpu().numpy()
-#
-# # Plot
-# plt.figure(figsize=(10, 5))
-# plt.plot(true_future, label="True Consumption")
-# plt.plot(forecast, label="Forecasted Consumption")
-# plt.title("One-Step Forecast vs True Consumption")
-# plt.xlabel("Time step")
-# plt.ylabel("Normalized Consumption")
-# plt.legend()
-# plt.grid(True)
-# plt.tight_layout()
-# plt.show()
-# # Plot
-# # plot_diffusion_forecast(forecast, ground_truth=true_future, column="Consumption")
-##############################################################################################################################
-
-
-#TRAIN MODEL
-##############################################################################################################################
+context_seq = create_sequences(test_tensor[-forecast_window], seq_length)
 model = DFBackbone(input_dim=input_dim, hidden_dim=hidden_dim, seq_dim=seq_length)
-model = model.to(device)
-pretrain_layers(model, train_sequences, total_epochs=20, device=device)
-df_training(model, train_sequences, validation_sequences, alpha_bar, K, epochs,
-            scaler, loss_type, device=device, save_path=save_path, one_shot_training=False)
-save_model(model, save_path, )
+model.load_state_dict(torch.load("trained_models/diffusion/H1.pt"))
 
-test_results = predict(model, test_sequences, alpha, alpha_bar, K, scaler, device=device)
-plot_test_predictions(test_results, scaler, save_path)
-# alpha_bar = alpha_bar.flip(0).contiguous()
-
-test_results2 = predict_with_uncertainty(model, test_sequences, alpha, alpha_bar, K, scaler, device=device)
-
-crps_score = compute_crps(
-    np.array(test_results2["xt_true"]),
-    np.array(test_results2["xt_pred_mean"]),
-    np.array(test_results2["xt_pred_std"])
-)
-
-print(f"CRPS Score: {crps_score:.6f}")
-
-logger.save_final_metrics(crps_score)
+device = 'cpu'
+alpha = alpha.to(device)
+alpha_bar = alpha_bar.to(device)
+# context_input = test_tensor[:-forecast_window]
+# true_future = test_tensor[-forecast_window:]
 #
-plot_predictions_with_uncertainty(test_results2, save_path)
+# context_seq = create_sequences(context_input, seq_length)
 
-
-
-# model = DFBackbone(input_dim=input_dim, hidden_dim=hidden_dim, seq_dim=seq_length)
-# model.load_state_dict(torch.load("plots/2025-05-20_16-54/df_model_2025-05-20_16-58-23.pt"))
-# model.to(device)
-# torch.cuda.empty_cache()
-# test_results = predict(model, test_sequences, alpha, alpha_bar, K, scaler, device=device)
-# plot_test_predictions(test_results, scaler, save_path)
+#
+torch.cuda.empty_cache()
 # forecast = sample_from_diffusion_with_context(
 #     model, context_seq, seq_length, forecast_window, hidden_dim, alpha, alpha_bar, K
 # )
-
-test_sequences = create_sequences(test_tensor[-forecast_window:], seq_length)
-predictions = sample_whole_test(model, test_sequences, alpha_bar, seq_length, input_dim, hidden_dim, K, device=device)
-# test_results2 = predict_with_uncertainty(model, test_sequences, alpha, alpha_bar, K, scaler, device=device)
-# plot_predictions_with_uncertainty(test_results2, save_path)
-
-true = flatten_overlapping_windows(test_sequences)
-forecast = flatten_overlapping_windows(predictions)
-
-forecast = get_from_sequence(forecast.detach(), column="Consumption").cpu().numpy()
-true_future = get_from_sequence(true, column="Consumption").cpu().numpy()
+context_window = 240
+context_seq = test_tensor[-context_window:]
+context_seq = ([context_seq[i:i+seq_length] for i in range(context_window - seq_length + 1)])
+predictions = generate_predictions(model=model, context_seq=context_seq, alpha=alpha,alpha_bar=alpha_bar, sequence_dim=seq_length,
+                                   feature_dim=input_dim, hidden_dim=hidden_dim, K=K, steps=24)
+true = test_tensor[-24:]
+# forecast = flatten_overlapping_windows(predictions)
+predictions = torch.cat(predictions, dim=0).detach().numpy()
+forecast = get_from_sequence(predictions, column="Production")
+true_future = get_from_sequence(true, column="Production").cpu().numpy()
 
 # Plot
 plt.figure(figsize=(10, 5))
@@ -179,6 +104,71 @@ plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+# Plot
+# plot_diffusion_forecast(forecast, ground_truth=true_future, column="Consumption")
+##############################################################################################################################
+
+
+#TRAIN MODEL
+##############################################################################################################################
+# train_tensor, val_tensor, test_tensor, scaler = preprocess_aep_dataset("./training sets/AEP_Wh.csv")
+# input_dim = train_tensor.shape[1]
+# train_sequences = create_sequences(train_tensor, seq_length)
+# test_sequences = create_sequences(test_tensor, seq_length)
+# validation_sequences = create_sequences(val_tensor, seq_length)
+#
+#
+#
+
+
+#
+# model = DFBackbone(input_dim=input_dim, hidden_dim=hidden_dim, seq_dim=seq_length)
+# model = model.to(device)
+# # pretrain_layers(model, train_sequences, total_epochs=20, device=device)
+# df_training(model, train_sequences, validation_sequences, alpha_bar, K, epochs,
+#             scaler, loss_type, device=device, save_path=save_path)
+# save_model(model, save_path, )
+#
+# test_results = predict(model, test_sequences, alpha, alpha_bar, K, scaler, device=device)
+# plot_test_predictions(test_results, scaler, save_path)
+#
+# test_results2 = predict_with_uncertainty(model, test_sequences, alpha, alpha_bar, K, scaler, device=device)
+#
+# crps_score = compute_crps(
+#     np.array(test_results2["xt_true"]),
+#     np.array(test_results2["xt_pred_mean"]),
+#     np.array(test_results2["xt_pred_std"])
+# )
+#
+# print(f"CRPS Score: {crps_score:.6f}")
+#
+# logger.save_final_metrics(crps_score)
+# #
+# plot_predictions_with_uncertainty(test_results2, save_path)
+#
+#
+# test_sequences = create_sequences(test_tensor[-forecast_window:], seq_length)
+# predictions = sample_whole_test(model, test_sequences, alpha_bar, seq_length, input_dim, hidden_dim, K, device=device)
+# # test_results2 = predict_with_uncertainty(model, test_sequences, alpha, alpha_bar, K, scaler, device=device)
+# # plot_predictions_with_uncertainty(test_results2, save_path)
+#
+# true = flatten_overlapping_windows(test_sequences)
+# forecast = flatten_overlapping_windows(predictions)
+#
+# forecast = get_from_sequence(forecast.detach(), column="Production").cpu().numpy()
+# true_future = get_from_sequence(true, column="Production").cpu().numpy()
+#
+# # Plot
+# plt.figure(figsize=(10, 5))
+# plt.plot(true_future, label="True Consumption")
+# plt.plot(forecast, label="Forecasted Consumption")
+# plt.title("One-Step Forecast vs True Consumption")
+# plt.xlabel("Time step")
+# plt.ylabel("Normalized Consumption")
+# plt.legend()
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
 ##############################################################################################################################
 
 #LSTM MODEL AND TRANSFORMER TRAINING
